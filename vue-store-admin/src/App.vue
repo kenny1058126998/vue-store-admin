7a04af01350dd4ae4b102c85af1be5e0a100a39c
z1058126998 <53297536+z1058126998@users.noreply.github.com>
2020-04-29 20:11:15 +0800

    初始化项目

---
+++ vue-store-admin/src/App.vue
@@ -0,0 +1,142 @@
+<template>
+  <div id="app">
+
+      <div>
+        <el-backtop></el-backtop>
+        <el-button>el-button</el-button>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+        <div>
+
+          <el-button>el-button</el-button>
+        </div>
+
+      </div>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'app',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
